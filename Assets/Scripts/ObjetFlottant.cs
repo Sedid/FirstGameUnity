8c4f3be9c23c27e88bbe3212ebb55fbb384bc7a3
Sedid <steven.cohen.t@gmail.com>
2019-11-18 01:33:55 +0100

    Ajout animation de flottement pour les objets à récupérer

---
+++ Assets/Scripts/ObjetFlottant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetFlottant : MonoBehaviour
+
+{
+    public AnimationCurve myCurve;
+   
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time / 4 % myCurve.length)), transform.position.z);
+    }
+}
