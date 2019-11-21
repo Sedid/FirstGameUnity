567a6055e040ba9b0099f27d410042a1ed99e750
Sedid <steven.cohen.t@gmail.com>
2019-11-20 19:43:55 -0800

    Obstacles sur les immeubles (en cours) + animation course sur le perso

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public bool obstacle = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            obstacle = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        obstacle = false;
+    }
+
+}
--- Assets/Scripts/bouge.cs
+++ Assets/Scripts/bouge.cs
@@ -25,38 +25,66 @@ public class bouge : MonoBehaviour
             return;
         }
         // Déplacement du personnage
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && (!Input.GetKey(KeyCode.LeftShift)))
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            if (!GetComponent<Obstacles>().obstacle)
+            {
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (!GetComponent<Obstacles>().obstacle)
+            {
+                transform.Translate(Vector3.forward * moveSpeed*2 * Time.deltaTime );
+            }
         }
 
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime/2);
+            if (!GetComponent<Obstacles>().obstacle)
+            {
+                transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime / 2);
+            }
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            if (!GetComponent<Obstacles>().obstacle)
+            {
+                transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            }
         }
 
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            if (!GetComponent<Obstacles>().obstacle)
+            {
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            }
         }
 
         // Animation walk lorsque fleches haut et bas sont appuyés
-        if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.DownArrow)))
+        if (Input.GetKey(KeyCode.UpArrow) && (!Input.GetKey(KeyCode.LeftShift)) || (Input.GetKey(KeyCode.DownArrow)))
         {
             anim.SetBool("anim_marcher", true);
             anim.SetBool("anim_repos", false);
+            anim.SetBool("anim_courir", false);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && (Input.GetKey(KeyCode.LeftShift)))
+        {
+            anim.SetBool("anim_marcher", false);
+            anim.SetBool("anim_repos", false);
+            anim.SetBool("anim_courir", true);
         }
         else
         {
             anim.SetBool("anim_marcher", false);
             anim.SetBool("anim_repos", true);
+            anim.SetBool("anim_courir", false);
         }
     }
 }
