34c473a3fa6b73d994e95e89ea7ca6814f56c482
Sedid <steven.cohen.t@gmail.com>
2019-11-21 16:59:38 +0100

    Boites de dialogues (en cours 2)

---
+++ Assets/Scripts/Dialogue_pnj_1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_pnj_1 : MonoBehaviour
+{
+    public GameObject dialogue;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pnj"))
+        {
+            dialogue.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("pnj"))
+        {
+            dialogue.SetActive(false);
+        }
+    }
+}
+
