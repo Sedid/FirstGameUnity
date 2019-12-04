bee94bd048cc6c1e2a93dc4af0af2ead13e73bd7
Sedid <steven.cohen.t@gmail.com>
2019-12-03 17:59:27 -0800

    Ajout du menu (curseur à ajouter) + résolution à agrandir ? + résolution bug obstacles

---
+++ Assets/Scripts/ActivationMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ActivationMenu : MonoBehaviour {
+	
+	public GameObject settings;
+	
+	public void StartGame ()
+	{
+        SceneManager.LoadScene("SceneBase");
+	}
+	public void Settings ()
+	{
+		settings.SetActive(!settings.activeSelf);
+	}
+	public void ExitGame ()
+	{
+		Application.Quit();
+	}
+}
+
--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -17,34 +17,21 @@ public class Obstacles : MonoBehaviour
     {
         if (collision.gameObject.tag == "obstacle")
         {
-            obstacle = true;
+            obstacle = true;  
         }
-        //if (collision.gameObject.name == "Portail")
-        //{
-        //    Debug.Log("Collision avec le portail !");
-        //    transform.position = new Vector3(4, 25.17f, 256);
-        //    if (transform.position == new Vector3(4, 25.17f, 256))
-        //    {
-        //        transform.Rotate(0, -292.6f, 0.51f);
-        //        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        //        joueurSpeed.moveSpeed = 15f;
-        //        joueurSpeed.turnSpeed = 25;
-        //    }
-        //}
-        //if (collision.gameObject.name == "PortailSortie")
-        //{
-        //    Debug.Log("Collision avec le portail de sortie !");
-        //    transform.position = new Vector3(3.682673f, 2.6985f, 5.914675f);
-        //    if (transform.position == new Vector3(3.682673f, 2.6985f, 5.914675f))
-        //    {
-        //        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-        //        transform.Rotate(0, -211.985f, 0);
-        //        joueurSpeed.moveSpeed = .15f;
-        //        joueurSpeed.turnSpeed = 75f;
-        //        rigidbodyJoueur.constraints = RigidbodyConstraints.FreezeAll;
-        //        rigidbodyJoueur.inertiaTensor = rigidbodyJoueur.inertiaTensor + new Vector3(0, 0, rigidbodyJoueur.inertiaTensor.z * 100);
-        //    }
-        //}
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            if (Mathf.Abs(Vector3.Angle(collision.contacts[0].normal, transform.forward)) > 90.0f)
+            {
+                obstacle = false;
+            }
+            else
+                obstacle = true;
+        }
     }
 
     private void OnCollisionExit(Collision collision)
